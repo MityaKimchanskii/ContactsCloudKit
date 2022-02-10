6c4f222edb1d9e358b15b994609a86e4d67a58a4
Dmitrii Kim <mitya_kim@icloud.com>
2022-02-09 17:27:20 -0800

    Initial Commit

---
+++ ContactsCloudKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsCloudKit
+//
+//  Created by Mitya Kim on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
